0cd872d5d3e627fd4f66101942ec8a1b11804c0b
Guang Yi <guangyitan1999@gmail.com>
2025-06-20 21:24:16 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+(function ($) {
+    "use strict";
+
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    // customer-feedbacks carousel
+    $(".customer-feedback-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+    // Reusable Image Modal Script
+    $('#imageModal').on('show.bs.modal', function (event) {
+        var triggerImage = $(event.relatedTarget);
+        var imageSrc = triggerImage.data('image-src');
+        var imageAlt = triggerImage.data('image-alt') || 'Image';
+        $('#modalImage').attr('src', imageSrc).attr('alt', imageAlt);
+    });
+
+})(jQuery);
